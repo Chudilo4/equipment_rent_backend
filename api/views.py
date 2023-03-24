fe7ccdf6cff98fad23cff690936039b5a3f1f3d7
chudilo4 <chipalinchik@gmail.com>
2023-03-24 11:48:01 +0500

    123

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from api.models import Equipment
 from api.serializers import EquipmentSerializer, FeedbackSerializer
 from rest_framework.response import Response
+import requests
 
 
 # Create your views here.
@@ -10,15 +11,17 @@ class EquipmentAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         equipment = Equipment.objects.all()
-        serializer = EquipmentSerializer(instance=equipment, many=True)
+        serializer = EquipmentSerializer(instance=equipment, many=True , context={'request': request})
         return Response(serializer.data, status.HTTP_200_OK)
 
 
 class FeedbackAPIView(APIView):
 
     def post(self, request, *args, **kwargs):
+        print(requests.post('https://api.telegram.org/bot5818251636:AAFIOC9Q3u3Q7cfMms0KqWcuhWXZUZTibE8/getCommand=timer'))
         serializer = FeedbackSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+
             return Response(serializer.data, status.HTTP_201_CREATED)
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)

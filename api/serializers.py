663b00adecf5b0c35d82f3f73fe3861f977408aa
chudilo4 <chipalinchik@gmail.com>
2023-03-31 15:18:30 +0500

    123

--- api/serializers.py
+++ api/serializers.py
@@ -12,7 +12,7 @@ class EquipmentSerializer(serializers.ModelSerializer):
 class FeedbackSerializer(serializers.ModelSerializer):
     fio = serializers.CharField(min_length=3, allow_null=False, allow_blank=False)
     phone_number = serializers.CharField(allow_blank=False, allow_null=False)
-    email = serializers.EmailField(allow_null=False, allow_blank=False)
+    email = serializers.EmailField()
     text = serializers.CharField(allow_blank=False, allow_null=False)
 
     class Meta:
